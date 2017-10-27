95f1a82809f80469dad2518ff659ee53d389b459
Timothy Miles <timothy.miles@guardvant.com>
2017-10-27 16:27:11 -0700

    Basic nab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,45 @@ export interface tab {
   id: String
 }
 
+export interface nav {
+  name: String, 
+  active: boolean,
+  displayText: String,
+  link: String, 
+  tabs: tab[],
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'app';
+export class AppComponent{
+  title = 'SWUVSA Apex-3';
   tabs: tab [] = [];
+  navigation: nav[];
   constructor(){
     this.tabs.push({name: "Main", tabNumber: 1, active: true, id: 'tab1'});
     this.tabs.push({name: "Second", tabNumber: 2, active: false, id: 'tab2'});
     this.tabs.push({name: "Third", tabNumber: 3, active: false, id: 'tab3'});
+    this.tabs.push({name: "Fourth", tabNumber: 4, active: false, id: 'tab4'});
+
+    this.navigation = [
+      {
+        name: "home", 
+        active: true,
+        displayText: "Home",
+        link: "/", 
+        tabs: [],
+      },
+      {
+        name: "apex-3", 
+        active: false,
+        displayText: "Apex-3",
+        link: "/register", 
+        tabs: [],
+      }
+    ]
   }
 }
